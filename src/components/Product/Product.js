c4c39cc1dd4ddd13888146512ffcd87ab3509d6d
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-08-24 04:18:11 +0600

    product detail

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './Product.css';
 
 const Product = (props) => {
+    const { name, img, seller, price, stock } = props.product
     return (
-        <div>
-            <h4> {props.product.name} </h4>
+        <div className="product">
+            <div className=''>
+                <img src={img} alt="" />
+            </div>
+            <div className=''>
+                <h4 className="product-name"> {name} </h4>
+                <br />
+                <p><small>by: {seller} </small></p>
+                <p>$ {price} </p>
+                <br />
+                <p><small> Only {stock} left in stock - Order soon </small></p>
+            </div>
         </div>
     );
 };
