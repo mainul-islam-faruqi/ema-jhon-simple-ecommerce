d732c444679ed6f5b4e6ac08261b7c82605d1a90
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-08-24 03:22:37 +0600

    product component with dynamic data

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData'
 import './Shop.css'
+import Product from '../Product/Product';
 
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
@@ -8,11 +9,9 @@ const Shop = () => {
     return (
         <div className="shop-container">
             <div className="product-container">
-            <ul>
                 {
-                    products.map(product => <li> {product.name} </li>)
+                    products.map(product => <Product product={product}>  </Product>)
                 }
-            </ul>
             </div>
             <div className="cart-container">
 
