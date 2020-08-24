c7c782bbbc47976e7f5bd97144624d5877886112
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-08-24 16:35:35 +0600

    click handler

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,12 +2,18 @@ import React, { useState } from 'react';
 import fakeData from '../../fakeData'
 import './Shop.css'
 import Product from '../Product/Product';
+import { faCartArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
     const [ products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([])
+    console.log(cart);
+
     function handleAddProduct(product){
         console.log('click marse', product)
+        const newCart = [...cart, product];
+        setCart(newCart);
     }
     return (
         <div className="shop-container">
@@ -18,6 +24,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
+                <h5> Order Summary: {cart.length}</h5>
 
             </div>
             
