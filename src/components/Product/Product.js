ffde04ea6cb0c0c0eb85565b45f5a50a4320f550
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-08-30 19:48:45 +0600

    passing Dynamic key to the url

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
+import { Link } from 'react-router-dom';
 
 const Product = (props) => {
-    const { name, img, seller, price, stock } = props.product
+    // console.log(props.product.key)
+    const { name, img, seller, price, stock,key } = props.product
     return (
         <div className="product">
             <div className='product-img'>
                 <img src={img} alt="" />
             </div>
             <div className='product-info'>
-                <h4 className="product-name"> {name} </h4>
+                <h4 className="product-name"> <Link to={"/product/"+key}>{name}</Link> </h4>
                 <p><small>by: {seller} </small></p>
                 <p>$ {price} </p>
                 <br />
