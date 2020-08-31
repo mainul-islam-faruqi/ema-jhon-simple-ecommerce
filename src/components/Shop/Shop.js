cc94d294df3cf3f456f07f6609be27d7c88bda9e
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-08-31 23:31:13 +0600

    add quantity in shop page

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -13,10 +13,22 @@ const Shop = () => {
     console.log(cart);
 
     function handleAddProduct(product){
-        const newCart = [...cart, product];
+        const toBeAddedKey = product.key;
+        const sameProduct = cart.find(pd=> pd.key === toBeAddedKey);
+        let count = 1;
+        let newCart;
+        if(sameProduct){
+            count = sameProduct.quantity + 1;
+            sameProduct.quantity = count;
+            const others = cart.filter(pd=> pd.key !== toBeAddedKey);
+            newCart = [...others,sameProduct];
+        }
+        else{
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
+
         setCart(newCart);
-        const sameProduct = newCart.filter(pd => pd.key === product.key)
-        const count = sameProduct.length;
         addToDatabaseCart(product.key, count);
     }
     return (
