40c17e2fc7c43ce990df570ae9d634f690864a2b
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-10-02 21:58:39 +0600

    load some products form server and inventory

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -20,15 +20,17 @@ const Shop = () => {
     useEffect(()=>{
         const savedCart = getDatabaseCart();
         const productKeys = Object.keys(savedCart);
-        if(products.length > 0){
-            const previousCart = productKeys.map( existingKey => {
-                const product = products.find( pd => pd.key === existingKey);
-                product.quantity = savedCart[existingKey];
-                return product;
-            } )
-            setCart(previousCart);
-        }
-    }, [products])
+        fetch('http://localhost:5000/productsByKeys', {
+            method: 'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(productKeys)
+        })
+        .then(res => res.json())
+        .then(data => setCart(data))
+
+    }, [])
 
     const handleAddProduct = (product) =>{
         const toBeAddedKey = product.key;
